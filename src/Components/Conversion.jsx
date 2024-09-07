d30dc8ef343aa236386e8bb181b7376b5b0654f4
Oscar-Jiram <oscarcasrto@gmail.com>
2024-09-07 18:57:22 -0400

    Graphics Styles added

--- src/Components/Conversion.jsx
+++ src/Components/Conversion.jsx
@@ -6,8 +6,8 @@ import Select from "react-select";
 function Conversion() {
   const [value1, setValue1] = useState(0);
   const [value2, setValue2] = useState(0);
-  const [currency1, setCurrency1] = useState({ value: "USD", label: "United States Dollar",image: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Flag_of_the_United_States.png' });
-  const [currency2, setCurrency2] = useState({ value: "MXN", label: "Mexican Peso",image: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg' });
+  const [currency1, setCurrency1] = useState({ value: "USD", label: "USD",image: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Flag_of_the_United_States.png' });
+  const [currency2, setCurrency2] = useState({ value: "MXN", label: "MXN",image: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg' });
   const [rates, setRates] = useState({});
 
   useEffect(() => {
@@ -42,36 +42,36 @@ function Conversion() {
   }, [value1, value2, currency1, currency2, rates]);
 
   const options = [
-    { value: 'AUD', label: 'Australian Dollar', image: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Flag_of_Australia.svg' },
-    { value: 'BGN', label: 'Bulgarian Lev', image: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Bulgaria.svg' },
-    { value: 'BRL', label: 'Brazilian Real', image: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Flag_of_Brazil.svg' },
-    { value: 'CAD', label: 'Canadian Dollar', image: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Flag_of_Canada.svg' },
-    { value: 'CHF', label: 'Swiss Franc', image: 'https://upload.wikimedia.org/wikipedia/commons/f/f3/Flag_of_Switzerland.svg' },
-    { value: 'CNY', label: 'Chinese Yuan', image: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Flag_of_the_People%27s_Republic_of_China.svg" },
-    { value: 'CZK', label: 'Czech Koruna', image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Flag_of_the_Czech_Republic.svg' },
-    { value: 'USD', label: 'United States Dollar', image: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Flag_of_the_United_States.png' },
-    { value: 'DKK', label: 'Danish Krone', image: 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Flag_of_Denmark.svg' },
-    { value: 'EUR', label: 'Euro', image: 'https://upload.wikimedia.org/wikipedia/commons/b/b7/Flag_of_Europe.svg' },
-    { value: 'GBP', label: 'British Pound', image: 'https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg' },
-    { value: 'HUF', label: 'Hungarian Forint', image: 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Flag_of_Hungary.svg' },
-    { value: 'IDR', label: 'Indonesian Rupiah', image: 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Flag_of_Indonesia.svg' },
-    { value: 'ILS', label: 'Israeli Shekel', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Flag_of_Israel.svg' },
-    { value: 'INR', label: 'Indian Rupee', image: 'https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg' },
-    { value: 'ISK', label: 'Icelandic Króna', image: 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Flag_of_Iceland.svg' },
-    { value: 'JPY', label: 'Japanese Yen', image: 'https://upload.wikimedia.org/wikipedia/en/9/9e/Flag_of_Japan.svg' },
-    { value: 'KRW', label: 'South Korean Won', image: 'https://upload.wikimedia.org/wikipedia/commons/0/09/Flag_of_South_Korea.svg' },
-    { value: 'MXN', label: 'Mexican Peso', image: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg' },
-    { value: 'MYR', label: 'Malaysian Ringgit', image: 'https://upload.wikimedia.org/wikipedia/commons/6/66/Flag_of_Malaysia.svg' },
-    { value: 'NOK', label: 'Norwegian Krone', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Norway.svg' },
-    { value: 'NZD', label: 'New Zealand Dollar', image: 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Flag_of_New_Zealand.svg' },
-    { value: 'PHP', label: 'Philippine Peso', image: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Flag_of_the_Philippines.svg' },
-    { value: 'PLN', label: 'Polish Zloty', image: 'https://upload.wikimedia.org/wikipedia/commons/1/12/Flag_of_Poland.svg' },
-    { value: 'RON', label: 'Romanian Leu', image: 'https://upload.wikimedia.org/wikipedia/commons/7/73/Flag_of_Romania.svg' },
-    { value: 'SEK', label: 'Swedish Krona', image: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Flag_of_Sweden.svg' },
-    { value: 'SGD', label: 'Singapore Dollar', image: 'https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Singapore.svg' },
-    { value: 'THB', label: 'Thai Baht', image: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Flag_of_Thailand.svg' },
-    { value: 'TRY', label: 'Turkish Lira', image: 'https://upload.wikimedia.org/wikipedia/commons/b/b4/Flag_of_Turkey.svg' },
-    { value: 'ZAR', label: 'South African Rand', image: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Flag_of_South_Africa.svg' },
+    { value: 'AUD', label: 'AUD', image: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Flag_of_Australia.svg' },
+    { value: 'BGN', label: 'BGN', image: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Bulgaria.svg' },
+    { value: 'BRL', label: 'BRL', image: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Flag_of_Brazil.svg' },
+    { value: 'CAD', label: 'CAD', image: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Flag_of_Canada.svg' },
+    { value: 'CHF', label: 'CHF', image: 'https://upload.wikimedia.org/wikipedia/commons/f/f3/Flag_of_Switzerland.svg' },
+    { value: 'CNY', label: 'CNY', image: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Flag_of_the_People%27s_Republic_of_China.svg" },
+    { value: 'CZK', label: 'CZK', image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Flag_of_the_Czech_Republic.svg' },
+    { value: 'USD', label: 'USD', image: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Flag_of_the_United_States.png' },
+    { value: 'DKK', label: 'DKK', image: 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Flag_of_Denmark.svg' },
+    { value: 'EUR', label: 'EUR', image: 'https://upload.wikimedia.org/wikipedia/commons/b/b7/Flag_of_Europe.svg' },
+    { value: 'GBP', label: 'GBP', image: 'https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg' },
+    { value: 'HUF', label: 'HUF', image: 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Flag_of_Hungary.svg' },
+    { value: 'IDR', label: 'IDR', image: 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Flag_of_Indonesia.svg' },
+    { value: 'ILS', label: 'ILS', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Flag_of_Israel.svg' },
+    { value: 'INR', label: 'INR', image: 'https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg' },
+    { value: 'ISK', label: 'ISK', image: 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Flag_of_Iceland.svg' },
+    { value: 'JPY', label: 'JPY', image: 'https://upload.wikimedia.org/wikipedia/en/9/9e/Flag_of_Japan.svg' },
+    { value: 'KRW', label: 'KRW', image: 'https://upload.wikimedia.org/wikipedia/commons/0/09/Flag_of_South_Korea.svg' },
+    { value: 'MXN', label: 'MXN', image: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg' },
+    { value: 'MYR', label: 'MYR', image: 'https://upload.wikimedia.org/wikipedia/commons/6/66/Flag_of_Malaysia.svg' },
+    { value: 'NOK', label: 'NOK', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Norway.svg' },
+    { value: 'NZD', label: 'NZD', image: 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Flag_of_New_Zealand.svg' },
+    { value: 'PHP', label: 'PHP', image: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Flag_of_the_Philippines.svg' },
+    { value: 'PLN', label: 'PLN', image: 'https://upload.wikimedia.org/wikipedia/commons/1/12/Flag_of_Poland.svg' },
+    { value: 'RON', label: 'RON', image: 'https://upload.wikimedia.org/wikipedia/commons/7/73/Flag_of_Romania.svg' },
+    { value: 'SEK', label: 'SEK', image: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Flag_of_Sweden.svg' },
+    { value: 'SGD', label: 'SGD', image: 'https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Singapore.svg' },
+    { value: 'THB', label: 'THB', image: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Flag_of_Thailand.svg' },
+    { value: 'TRY', label: 'TRY', image: 'https://upload.wikimedia.org/wikipedia/commons/b/b4/Flag_of_Turkey.svg' },
+    { value: 'ZAR', label: 'ZAR', image: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Flag_of_South_Africa.svg' },
   ];
 
   const deleteContent = (e) => {
@@ -106,17 +106,12 @@ function Conversion() {
     }
   };
 
-  const imprimir = () => {
-    console.log("currency1:", currency1);
-    console.log("currency2:", currency2);
-    console.log("rates:", rates);
-  };
-
   return (
     <div>
       
         <div className="menu-contenedor">
           <Select
+            isSearchable={false} 
             value={currency1}
             onChange={handleCurrency1Change}
             options={options}
@@ -138,6 +133,7 @@ function Conversion() {
           <br />
           <div className="flechas">↑ ↓</div>
           <Select
+            isSearchable={false} 
             value={currency2}
             onChange={handleCurrency2Change}
             options={options}
@@ -156,7 +152,6 @@ function Conversion() {
             value={value2 !== "" ? value2 : ""}
             onChange={handleValue2Change}
           />
-          <button onClick={imprimir}>Print</button>
         </div>
       </div>
     
--- src/Components/Graficos.jsx
+++ src/Components/Graficos.jsx
@@ -1,9 +1,271 @@
-import React from 'react'
+import { Link, Route, Routes, Navigate } from "react-router-dom";
+import ReactDOM from "react-dom";
+import React, { useState, useEffect, useRef } from "react";
+import Select from "react-select";
+import { createChart } from "lightweight-charts";
 
-export default function Graficos() {
+function Graficos() {
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const [timeRange, setTimeRange] = useState("1W");
+  const [currency1, setCurrency1] = useState({
+    value: "USD",
+    label: "USD",
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/de/Flag_of_the_United_States.png",
+  });
+  const [currency2, setCurrency2] = useState({
+    value: "MXN",
+    label: "MXN",
+    image: "https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg",
+  });
+  const [rates, setRates] = useState({});
+  const [data, setData] = useState([]);
+  const chartContainerRef = useRef(null);
+  const chartRef = useRef(null);
+  const areaSeriesRef = useRef(null);
+
+  // Función para calcular las fechas según el rango de tiempo seleccionado
+  const calculateDateRange = (range) => {
+    const endDate = new Date();
+    let startDate = new Date();
+    switch (range) {
+      case "1W":
+        startDate.setDate(endDate.getDate() - 7);
+        break;
+      case "1M":
+        startDate.setMonth(endDate.getMonth() - 1);
+        break;
+      case "1Y":
+        startDate.setFullYear(endDate.getFullYear() - 1);
+        break;
+      case "5Y":
+        startDate.setFullYear(endDate.getFullYear() - 5);
+        break;
+      default:
+        startDate = endDate;
+    }
+    return { startDate, endDate };
+  };
+
+  // useEffect para inicializar el gráfico
+  useEffect(() => {
+    if (chartContainerRef.current) {
+      chartRef.current = createChart(chartContainerRef.current, {
+        layout: { textColor: 'black', background: { type: 'solid', color: 'white' } },
+        width: chartContainerRef.current.clientWidth,
+        height: chartContainerRef.current.clientHeight,
+        rightPriceScale: {
+          visible: true,
+        },
+        timeScale: {
+          timeVisible: true,
+          secondsVisible: false,
+        },
+      });
+      areaSeriesRef.current = chartRef.current.addAreaSeries({
+        lineColor: "#2962FF",
+        topColor: "#2962FF",
+        bottomColor: "rgba(41, 98, 255, 0.28)",
+      });
+    }
+
+    return () => {
+      if (chartRef.current) {
+        chartRef.current.remove();
+        chartRef.current = null;
+      }
+    };
+  }, []);
+
+  // useEffect para actualizar los datos al cambiar el rango de tiempo
+  useEffect(() => {
+    const { startDate, endDate } = calculateDateRange(timeRange);
+    const fetchData = async () => {
+      try {
+        // Formatear fechas como YYYY-MM-DD
+        const formatDate = (date) => date.toISOString().split('T')[0];
+
+        // Crear un array de fechas distribuidas uniformemente entre startDate y endDate
+        const dates = Array.from({ length: 10 }, (_, i) => {
+          const current = new Date(startDate.getTime() + ((endDate.getTime() - startDate.getTime()) / 9) * i);
+          return formatDate(current);
+        });
+
+        // Hacer una petición para cada fecha
+        const promises = dates.map(date =>
+          fetch(`https://api.frankfurter.app/${date}?from=${currency1.value}&to=${currency2.value}`)
+            .then(res => res.json())
+        );
+
+        // Esperar a todas las peticiones y extraer los valores de currency2
+        const results = await Promise.all(promises);
+        const newData = results.map((result, i) => ({
+          time: new Date(dates[i]).getTime() / 1000, // convertir a timestamp Unix
+          value: result.rates[currency2.value],
+        }));
+
+        // Ordena los datos por tiempo
+        newData.sort((a, b) => a.time - b.time);
+
+        // Actualizar data y el gráfico
+        setData(newData);
+        if (areaSeriesRef.current) {
+          areaSeriesRef.current.setData(newData);
+        }
+
+        console.log("Fechas:", dates);
+        console.log("Valores:", newData.map(d => d.value));
+
+      } catch (error) {
+        console.error('Error al obtener los datos:', error);
+      }
+    };
+
+    fetchData();
+  }, [timeRange, currency1, currency2]);
+
+  useEffect(() => {
+    const url = `https://api.frankfurter.app/latest?from=${currency1.value}&to=${currency2.value}`;
+
+    const fetchRates = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setRates(data.rates);
+      } catch (error) {
+        console.error("Error al obtener las tarifas de cambio:", error);
+      }
+    };
+
+    fetchRates();
+  }, [currency1, currency2]);
+
+  useEffect(() => {
+    if (rates[currency2.value]) {
+      const rate1to2 = rates[currency2.value];
+      const rate2to1 = 1 / rate1to2;
+      if (document.activeElement.name === "value1") {
+        const convertedValue = (value1 * rate1to2).toFixed(2);
+        setValue2(convertedValue);
+      } else if (document.activeElement.name === "value2") {
+        const convertedValue = (value2 * rate2to1).toFixed(2);
+        setValue1(convertedValue);
+      }
+    }
+  }, [value1, value2, currency1, currency2, rates]);
+
+  const options = [
+    { value: 'AUD', label: 'AUD', image: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Flag_of_Australia.svg' },
+    { value: 'BGN', label: 'BGN', image: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Bulgaria.svg' },
+    { value: 'BRL', label: 'BRL', image: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Flag_of_Brazil.svg' },
+    { value: 'CAD', label: 'CAD', image: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Flag_of_Canada.svg' },
+    { value: 'CHF', label: 'CHF', image: 'https://upload.wikimedia.org/wikipedia/commons/f/f3/Flag_of_Switzerland.svg' },
+    { value: 'CNY', label: 'CNY', image: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Flag_of_the_People%27s_Republic_of_China.svg" },
+    { value: 'CZK', label: 'CZK', image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Flag_of_the_Czech_Republic.svg' },
+    { value: 'USD', label: 'USD', image: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Flag_of_the_United_States.png' },
+    { value: 'DKK', label: 'DKK', image: 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Flag_of_Denmark.svg' },
+    { value: 'EUR', label: 'EUR', image: 'https://upload.wikimedia.org/wikipedia/commons/b/b7/Flag_of_Europe.svg' },
+    { value: 'GBP', label: 'GBP', image: 'https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg' },
+    { value: 'HUF', label: 'HUF', image: 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Flag_of_Hungary.svg' },
+    { value: 'IDR', label: 'IDR', image: 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Flag_of_Indonesia.svg' },
+    { value: 'ILS', label: 'ILS', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Flag_of_Israel.svg' },
+    { value: 'INR', label: 'INR', image: 'https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg' },
+    { value: 'ISK', label: 'ISK', image: 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Flag_of_Iceland.svg' },
+    { value: 'JPY', label: 'JPY', image: 'https://upload.wikimedia.org/wikipedia/en/9/9e/Flag_of_Japan.svg' },
+    { value: 'KRW', label: 'KRW', image: 'https://upload.wikimedia.org/wikipedia/commons/0/09/Flag_of_South_Korea.svg' },
+    { value: 'MXN', label: 'MXN', image: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg' },
+    { value: 'MYR', label: 'MYR', image: 'https://upload.wikimedia.org/wikipedia/commons/6/66/Flag_of_Malaysia.svg' },
+    { value: 'NOK', label: 'NOK', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Norway.svg' },
+    { value: 'NZD', label: 'NZD', image: 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Flag_of_New_Zealand.svg' },
+    { value: 'PHP', label: 'PHP', image: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Flag_of_the_Philippines.svg' },
+    { value: 'PLN', label: 'PLN', image: 'https://upload.wikimedia.org/wikipedia/commons/1/12/Flag_of_Poland.svg' },
+    { value: 'RON', label: 'RON', image: 'https://upload.wikimedia.org/wikipedia/commons/7/73/Flag_of_Romania.svg' },
+    { value: 'SEK', label: 'SEK', image: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Flag_of_Sweden.svg' },
+    { value: 'SGD', label: 'SGD', image: 'https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Singapore.svg' },
+    { value: 'THB', label: 'THB', image: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Flag_of_Thailand.svg' },
+    { value: 'TRY', label: 'TRY', image: 'https://upload.wikimedia.org/wikipedia/commons/b/b4/Flag_of_Turkey.svg' },
+    { value: 'ZAR', label: 'ZAR', image: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Flag_of_South_Africa.svg' },
+  ];
+
+  const switchOptions = () => {
+    const temporaryCurrency1 = currency1;
+    setCurrency1(currency2);
+    setCurrency2(temporaryCurrency1);
+  };
+
+  const handleCurrency1Change = (selectedOption) => {
+    setCurrency1(selectedOption);
+  };
+
+  const handleCurrency2Change = (selectedOption) => {
+    setCurrency2(selectedOption);
+  };
+
+  const handleConversion = (value, setValue, fromCurrency, toCurrency) => {
+    if (rates[toCurrency]) {
+      const rate = rates[toCurrency];
+      const convertedValue = (value * rate).toFixed(2);
+      setValue(convertedValue);
+    }
+  };
+
   return (
-    <div className='menu-contenedor'>
-      Graficos
+    <div>
+      <div className="menu-contenedor">
+      <div className="range-container" style={{ marginTop: "20px" }}>
+        <button className="range" onClick={() => setTimeRange("1W")}>1W</button>
+        <button className="range" onClick={() => setTimeRange("1M")}>1M</button>
+        <button className="range" onClick={() => setTimeRange("1Y")}>1Y</button>
+        <button className="range" onClick={() => setTimeRange("5Y")}>5Y</button>
+      </div><br/>
+      <div className="bellowRange">
+  <div className="select-container">
+    <Select
+      value={currency1}
+      onChange={handleCurrency1Change}
+      options={options}
+      formatOptionLabel={({ label, image }) => (
+        <div style={{ display: "block", color: "black", alignItems: "center" }}>
+          <img src={image} alt={label} style={{ width: 30, height: 22, marginRight: 10 }} />
+          <span>{label}</span>
+        </div>
+      )}
+      className="selection2"
+    />
+    <div className="flechas" onClick={switchOptions}>
+    →
     </div>
-  )
+    <Select
+      value={currency2}
+      onChange={handleCurrency2Change}
+      options={options}
+      formatOptionLabel={({ label, image }) => (
+        <div style={{ display: "block", color: "black", alignItems: "center" }}>
+          <img src={image} alt={label} style={{ width: 30, height: 22, marginRight: 10 }} />
+          <span>{label}</span>
+        </div>
+      )}
+      className="selection2"
+    />
+  </div><br />
+  <div className="canvaContainer">
+  <div
+  ref={chartContainerRef}
+  style={{
+    width: "80%",             /* Proporción de ancho para mantenerlo responsivo */
+    maxWidth: "750px",        /* Máximo ancho */
+    minWidth: "200px",        /* Mínimo ancho */
+    minHeight: "150px",       /* Mínimo alto */
+    height: "30vh",           /* Altura basada en viewport para mantenerlo responsivo */
+    margin: "0 auto",         /* Centra horizontalmente */
+    display: "block",         /* Display block para asegurar que los márgenes funcionen */
+  }}
+></div>
+</div>
+</div>
+      </div>
+    </div>
+  );
 }
+
+export default Graficos;
